23d54dc0b18d8f4dfb7bec322935038753863e00
mohamad massoidi <mohamadmassoudi90@gmail.com>
2025-07-29 06:50:25 +0330

    use efect hook and map data hotel in locayion component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import LocationList from "./components/LocationList/LocationList";
 
 function App() {
   return (
     <div>
       <Header />
+      <LocationList />
     </div>
   );
 }
